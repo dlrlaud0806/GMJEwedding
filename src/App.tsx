95ccbb0551e4dd595e8af4c5c241acf47ed11b32
gmlee <gmlee@ntels.com>
2023-06-02 14:43:21 +0900

    modified copyright

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ function App() {
           textAlign: 'center',
         }}
       >
-        Copyright © 2021 KyuHyuk Lee
+        Copyright © 2023 Gimyeong Lee
       </Footer>
     </main>
   );
